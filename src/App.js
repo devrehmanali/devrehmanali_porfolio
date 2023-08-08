c56d89353d2dfb7230c8dfdedeeafb40902ab922
devrehmanali <dev.rehmanali@gmail.com>
2023-08-08 22:29:45 +0500

    updated homepage

--- src/App.js
+++ src/App.js
@@ -36,11 +36,10 @@ const App = () => {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/devrehmanali_porfolio" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/devrehmanali_porfolio" />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
       </div>
